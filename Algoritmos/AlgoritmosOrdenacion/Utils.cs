4e374501aac7f5d81edabba85e3d8aa5720916e0
KFuee <jbisazac@icloud.com>
2022-04-20 13:01:26 +0200

    Métodos para pedir un número e imprimir resultado por consola

---
+++ Algoritmos/AlgoritmosOrdenacion/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AlgoritmosOrdenacion
+{
+    class Utils
+    {
+        public static int pedirNumero(string mensaje = "")
+        {
+            Console.Write(mensaje.Length > 0 ? mensaje : "Introduce un número: ");
+
+            return int.Parse(Console.ReadLine());
+        }
+
+        public static void imprimirResultado(string metodo, int[] resultado)
+        {
+            Console.WriteLine(string.Format("El resultado del {0} es:", metodo));
+
+            foreach (var item in resultado)
+            {
+                Console.Write(item + " ");
+            }
+
+            // Deja espacio en blanco al final
+            Console.WriteLine();
+        }
+    }
+}
