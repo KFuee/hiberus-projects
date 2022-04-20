36e456c46a991224a06ea5c93617b32b41b0d434
KFuee <jbisazac@icloud.com>
2022-04-20 10:21:27 +0200

    Ejercicio 10 de algoritmos recursivos

---
+++ Algoritmos/AlgoritmosRecursivos/AlgoritmosRecursivos/Ejercicios/Ejercicio10.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace AlgoritmosRecursivos.Ejercicios
+{
+    class Ejercicio10
+    {
+        public static void ejecutar()
+        {
+            // Pide los datos por consola
+            int numeroDeDatos = Utils.pedirNumero("Introduce el tamaño del vector: ");
+
+            int[] vectorNumeros = new int[numeroDeDatos];
+
+            for (int i = 0; i < numeroDeDatos; i++)
+            {
+                int nuevoNumero = Utils.pedirNumero(
+                    string.Format("Introduce el número {0}: ", i + 1));
+                vectorNumeros[i] = nuevoNumero;
+            }
+
+            // Obtiene el número mayor de los elementos del vector introducido
+            int resultadoNumeroMayor = numeroMayorVector(vectorNumeros, numeroDeDatos - 1);
+            Console.WriteLine("Resultado: " + resultadoNumeroMayor);
+        }
+
+        private static int numeroMayorVector(int[] vector, int length)
+        {
+            if (length == 1)
+            {
+                return vector[0] > vector[1] ? vector[0] : vector[1];
+            }
+
+            return vector[length] > numeroMayorVector(vector, length - 1) ?
+                vector[length] : numeroMayorVector(vector, length - 1);
+        }
+    }
+}
