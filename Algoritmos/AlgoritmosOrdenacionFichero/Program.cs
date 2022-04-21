07b2746e1ba4009515c4a65cc0dd8bdf781a3e5a
KFuee <jbisazac@icloud.com>
2022-04-21 14:09:09 +0200

    Calcular tiempo de ejecución del bubble sort

--- Algoritmos/AlgoritmosOrdenacionFichero/Program.cs
+++ Algoritmos/AlgoritmosOrdenacionFichero/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.IO;
 using System.Diagnostics;
+using System.Linq;
+
+// using métodos de ordenación
+using AlgoritmosOrdenacionLib.Metodos;
 
 namespace AlgoritmosOrdenacionFichero
 {
     class Program
     {
         static Stopwatch timerLectura = new Stopwatch();
+        
+        // Timer BubbleSort
+        static Stopwatch timerBubbleSort = new Stopwatch();
+        static long[] tiemposBubbleSort = new long[10];
 
         private static int[] leerNumerosDeFichero()
         {
@@ -28,14 +36,53 @@ namespace AlgoritmosOrdenacionFichero
             return numerosParsed;
         }
 
+        private static void ordenarBubbleSort(int[] vector)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // Empieza a contar el tiempo de ejecución
+                timerBubbleSort.Start();
+
+                new BubbleSort().algoritmo(vector);
+
+                // Para de contar el tiempo de ejecución
+                timerBubbleSort.Stop();
+
+                // Añade el tiempo de ejecución de la iteración al vector
+                tiemposBubbleSort[i] = timerBubbleSort.ElapsedMilliseconds;
+                // Resetea el contador
+                timerBubbleSort.Reset();
+            }
+        }
+
+        static void imprimirResultado(string tipo, long[] tiempos)
+        {
+            Console.Write(string.Format("Números ordenados con {0} en: ", tipo));
+            foreach (var ms in tiempos)
+            {
+                Console.Write(ms + "ms ");
+            }
+
+            // Espacio en blanco
+            Console.WriteLine();
+
+            Console.WriteLine("Media de las 10 iteraciones con {0}: {1}ms", tipo, tiempos.Sum() / 10);
+        }
+
+        // TODO: Al finalizar las iteracione obtener el que ha tardado menos y utilizarlo para escribir en el txt
         static void Main(string[] args)
         {
             // Lee los números del fichero numbers.txt
             int[] numeros = leerNumerosDeFichero();
 
-            // Imprime el tiempo tomado en ejecutar leerNumerosDeFichero
+            // Ejecuta los métodos de ordenación
+            ordenarBubbleSort(numeros);
+
+            // Imprime los tiempos de ejecución
             Console.WriteLine(
                 string.Format("Números del fichero leídos en {0}ms", timerLectura.ElapsedMilliseconds));
+            imprimirResultado("Bubble Sort", tiemposBubbleSort);
+            
         }
     }
 }
--- Algoritmos/AlgoritmosOrdenacionMenu/Program.cs
+++ Algoritmos/AlgoritmosOrdenacionMenu/Program.cs
@@ -1,37 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using Algoritmos = AlgoritmosOrdenacionLib;
+
+using AlgoritmosOrdenacionLib;
+using AlgoritmosOrdenacionLib.Metodos;
 
 namespace AlgoritmosOrdenacionMenu
 {
     class Program
     {
-        static Dictionary<int, Algoritmos.Metodo> metodos = 
-            new Dictionary<int, Algoritmos.Metodo>
+        static Dictionary<int, Metodo> metodos = new Dictionary<int, Metodo>
         {
-            { 1, new Algoritmos.Metodos.BubbleSort() },
-            { 2, new Algoritmos.Metodos.QuickSort() },
-            { 3, new Algoritmos.Metodos.SelectionSort() },
-            { 4, new Algoritmos.Metodos.InsertionSort() }
+            { 1, new BubbleSort() },
+            { 2, new QuickSort() },
+            { 3, new SelectionSort() },
+            { 4, new InsertionSort() }
         };
 
         static void Main(string[] args)
         {
-            foreach (KeyValuePair<int, Algoritmos.Metodo> metodo in metodos)
+            foreach (KeyValuePair<int, Metodo> metodo in metodos)
             {
                 Console.WriteLine(string.Format("{0}. {1}", metodo.Key, metodo.Value.nombre));
             }
 
             // Pregunta al usuario la acción que desea ejecutar
-            int metodoUsuario = Algoritmos.Utils.pedirNumero("Selecciona la acción a ejecutar: ");
+            int metodoUsuario = Utils.pedirNumero("Selecciona la acción a ejecutar: ");
 
-            int sizeVector = Algoritmos.Utils.pedirNumero("Introduce el tamaño del vector: ");
+            int sizeVector = Utils.pedirNumero("Introduce el tamaño del vector: ");
             int[] vectorUsuario = new int[sizeVector];
 
             // Pregunta al usuario por los números que quiere introducir en el vector
             for (int i = 0; i < sizeVector; i++)
             {
-                vectorUsuario[i] = Algoritmos.Utils.pedirNumero(
+                vectorUsuario[i] = Utils.pedirNumero(
                     string.Format("Introduce el número de la posición {0}: ", i));
             }
 
