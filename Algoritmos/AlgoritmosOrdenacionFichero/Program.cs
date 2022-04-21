069b4156a6d0352150e9b4e5c8f76cda008705a8
KFuee <jbisazac@icloud.com>
2022-04-21 12:54:52 +0200

    Cargar números del txt en array calculando tiempo

---
+++ Algoritmos/AlgoritmosOrdenacionFichero/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace AlgoritmosOrdenacionFichero
+{
+    class Program
+    {
+        static Stopwatch timerLectura = new Stopwatch();
+
+        private static int[] leerNumerosDeFichero()
+        {
+            // Empieza a contar el tiempo de ejecución
+            timerLectura.Start();
+
+            string fichero = File.ReadAllText("numbers.txt");
+            string[] numeros = fichero.Split(' ');
+
+            int[] numerosParsed = new int[numeros.Length];
+            for (int i = 0; i < numeros.Length - 1; i++)
+            {
+                numerosParsed[i] = int.Parse(numeros[i]);
+            }
+
+            // Para de contar el tiempo de ejecución
+            timerLectura.Stop();
+
+            return numerosParsed;
+        }
+
+        static void Main(string[] args)
+        {
+            // Lee los números del fichero numbers.txt
+            int[] numeros = leerNumerosDeFichero();
+
+            // Imprime el tiempo tomado en ejecutar leerNumerosDeFichero
+            Console.WriteLine(
+                string.Format("Números del fichero leídos en {0}ms", timerLectura.ElapsedMilliseconds));
+        }
+    }
+}
---
+++ Algoritmos/AlgoritmosOrdenacionFichero/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("AlgoritmosOrdenacionFichero")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlgoritmosOrdenacionFichero")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("8bffe7c2-8c83-4adf-9e80-047c4f83fc6f")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
