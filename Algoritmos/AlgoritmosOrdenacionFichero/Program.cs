be036c4668f0e22184b694437830bd9f76e4a96b
KFuee <jbisazac@icloud.com>
2022-04-21 14:30:11 +0200

    Nueva forma de obtener y mostrar los tiempos

--- Algoritmos/AlgoritmosOrdenacionFichero/Program.cs
+++ Algoritmos/AlgoritmosOrdenacionFichero/Program.cs
@@ -4,22 +4,19 @@ using System.Diagnostics;
 using System.Linq;
 
 // using métodos de ordenación
+using AlgoritmosOrdenacionLib;
 using AlgoritmosOrdenacionLib.Metodos;
 
 namespace AlgoritmosOrdenacionFichero
 {
     class Program
     {
-        static Stopwatch timerLectura = new Stopwatch();
-        
-        // Timer BubbleSort
-        static Stopwatch timerBubbleSort = new Stopwatch();
-        static long[] tiemposBubbleSort = new long[10];
+        static Stopwatch timer = new Stopwatch();
 
         private static int[] leerNumerosDeFichero()
         {
             // Empieza a contar el tiempo de ejecución
-            timerLectura.Start();
+            timer.Start();
 
             string fichero = File.ReadAllText("numbers.txt");
             string[] numeros = fichero.Split(' ');
@@ -31,28 +28,9 @@ namespace AlgoritmosOrdenacionFichero
             }
 
             // Para de contar el tiempo de ejecución
-            timerLectura.Stop();
+            timer.Stop();
 
             return numerosParsed;
-        }
-
-        private static void ordenarBubbleSort(int[] vector)
-        {
-            for (int i = 0; i < 10; i++)
-            {
-                // Empieza a contar el tiempo de ejecución
-                timerBubbleSort.Start();
-
-                new BubbleSort().algoritmo(vector);
-
-                // Para de contar el tiempo de ejecución
-                timerBubbleSort.Stop();
-
-                // Añade el tiempo de ejecución de la iteración al vector
-                tiemposBubbleSort[i] = timerBubbleSort.ElapsedMilliseconds;
-                // Resetea el contador
-                timerBubbleSort.Reset();
-            }
         }
 
         static void imprimirResultado(string tipo, long[] tiempos)
@@ -67,6 +45,32 @@ namespace AlgoritmosOrdenacionFichero
             Console.WriteLine();
 
             Console.WriteLine("Media de las 10 iteraciones con {0}: {1}ms", tipo, tiempos.Sum() / 10);
+
+            // Espacio en blanco
+            Console.WriteLine();
+        }
+
+        private static long[] obtenerTiemposDeEjecucion(Metodo Metodo, int[] numeros)
+        {
+            long[] tiemposEjecucion = new long[10];
+            for (int i = 0; i < 10; i++)
+            {
+                // Empieza a contar el tiempo de ejecución
+                timer.Start();
+
+                // Ordena el vector de números
+                Metodo.algoritmo(numeros);
+
+                // Para de contar el tiempo de ejecución
+                timer.Stop();
+
+                // Añade el tiempo de ejecución de la iteración al vector
+                tiemposEjecucion[i] = timer.ElapsedMilliseconds;
+                // Resetea el contador
+                timer.Reset();
+            }
+
+            return tiemposEjecucion;
         }
 
         // TODO: Al finalizar las iteracione obtener el que ha tardado menos y utilizarlo para escribir en el txt
@@ -76,13 +80,18 @@ namespace AlgoritmosOrdenacionFichero
             int[] numeros = leerNumerosDeFichero();
 
             // Ejecuta los métodos de ordenación
-            ordenarBubbleSort(numeros);
+            long[] tiemposEjecucionBubbleSort = obtenerTiemposDeEjecucion(new BubbleSort(), numeros);
 
             // Imprime los tiempos de ejecución
             Console.WriteLine(
-                string.Format("Números del fichero leídos en {0}ms", timerLectura.ElapsedMilliseconds));
-            imprimirResultado("Bubble Sort", tiemposBubbleSort);
-            
+                string.Format("Números del fichero leídos en {0}ms", timer.ElapsedMilliseconds));
+            // Resetea el contador
+            timer.Reset();
+
+            // Espacio en blanco
+            Console.WriteLine();
+           
+            imprimirResultado("Bubble Sort", tiemposEjecucionBubbleSort);
         }
     }
 }
