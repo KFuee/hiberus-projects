758b6ea2062510698b9e362cdc8ae308721be9a2
KFuee <jbisazac@icloud.com>
2022-04-22 13:19:28 +0200

    Algoritmo para buscar palabra en específico

---
+++ Algoritmos/AlgoritmosBusqueda/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace AlgoritmosBusqueda
+{
+    class Program
+    {
+        private static int busquedaPalabra(string palabra, string[] texto)
+        {
+            int pos = 0;
+
+            for (int i = 0; i < texto.Length - 1; i++)
+            {
+                if (texto[i].Equals(palabra))
+                {
+                    pos = i;
+                }
+            }
+
+            return pos;
+        }
+
+        static void Main(string[] args)
+        {
+            string fichero = File.ReadAllText("search.txt");
+            string[] texto = fichero.Split(' ', '\n');
+
+            int posicionPalabra = busquedaPalabra("perro", texto);
+            System.Console.WriteLine(
+                string.Format("Palabra encontrada en la posición: {0}", posicionPalabra));
+        }
+    }
+}
