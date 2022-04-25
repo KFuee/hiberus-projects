60398e61d2932f22d07efdce38f84c24a9c4b85a
KFuee <jbisazac@icloud.com>
2022-04-25 13:11:08 +0200

    Controladores y vistas por defecto para users y social networks

---
+++ SocialRich/SocialRich/Controllers/SocialNetworksController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SocialRich.Models;
+
+namespace SocialRich.Controllers
+{
+    public class SocialNetworksController : Controller
+    {
+        private Modelo db = new Modelo();
+
+        // GET: SocialNetworks
+        public ActionResult Index()
+        {
+            return View(db.SocialNetworks.ToList());
+        }
+
+        // GET: SocialNetworks/Details/5
+        public ActionResult Details(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SocialNetwork socialNetwork = db.SocialNetworks.Find(id);
+            if (socialNetwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socialNetwork);
+        }
+
+        // GET: SocialNetworks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SocialNetworks/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Url")] SocialNetwork socialNetwork)
+        {
+            if (ModelState.IsValid)
+            {
+                db.SocialNetworks.Add(socialNetwork);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(socialNetwork);
+        }
+
+        // GET: SocialNetworks/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SocialNetwork socialNetwork = db.SocialNetworks.Find(id);
+            if (socialNetwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socialNetwork);
+        }
+
+        // POST: SocialNetworks/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Url")] SocialNetwork socialNetwork)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(socialNetwork).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(socialNetwork);
+        }
+
+        // GET: SocialNetworks/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SocialNetwork socialNetwork = db.SocialNetworks.Find(id);
+            if (socialNetwork == null)
+            {
+                return HttpNotFound();
+            }
+            return View(socialNetwork);
+        }
+
+        // POST: SocialNetworks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(long id)
+        {
+            SocialNetwork socialNetwork = db.SocialNetworks.Find(id);
+            db.SocialNetworks.Remove(socialNetwork);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
