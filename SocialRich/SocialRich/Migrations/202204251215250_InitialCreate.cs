80c722d189db2fd13ce798f31ea1d10b0c197869
KFuee <jbisazac@icloud.com>
2022-04-25 14:52:30 +0200

    Añadir datos de prueba con seeder

---
+++ SocialRich/SocialRich/Migrations/202204251215250_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿namespace SocialRich.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.SocialNetworks",
+                c => new
+                    {
+                        Id = c.Long(nullable: false, identity: true),
+                        Name = c.String(),
+                        Url = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                        Surname = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.SocialNetworks");
+        }
+    }
+}
---
+++ SocialRich/SocialRich/Migrations/Configuration.cs
@@ -0,0 +1,36 @@
+﻿namespace SocialRich.Migrations
+{
+    using SocialRich.Models;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SocialRich.Models.SocialContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "SocialRich.Models.SocialContext";
+        }
+
+        protected override void Seed(SocialRich.Models.SocialContext context)
+        {
+            // Añade usuarios de prueba con el Seeder
+            List<User> users = new List<User>
+            {
+               new User { Name = "Alfonso", Surname = "Garcia" },
+               new User { Name = "Pedro", Surname = "Perez" },
+               new User { Name = "Marta", Surname = "Sancho" }
+            };
+            users.ForEach((user) => context.Users.AddOrUpdate(user));
+
+            // Añade redes sociales de prueba con el Seeder
+            List<SocialNetwork> socialNetworks = new List<SocialNetwork>
+            {
+                new SocialNetwork { Name = "WhatsApp", Url = "https://whatsapp.com" },
+                new SocialNetwork {Name = "Instagram", Url = "https://instagram.com" },
+                new SocialNetwork {Name = "Snapchat", Url = "https://snapchat.com" }
+            };
+            socialNetworks.ForEach((socialNetwork) => context.SocialNetworks.AddOrUpdate(socialNetwork));
+        }
+    }
+}
